9a5aa9821303cb4e008d5e4f42ce0bb96bebc755
elizabeth-anderson <student@elizabeths-computer.local>
2017-08-31 12:03:56 -0500

    beth commit

--- simonGame/ViewController.swift
+++ simonGame/ViewController.swift
@@ -12,31 +12,72 @@ class ViewController: UIViewController
 {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet var colorDisplays: [UIView]!
-   
-
-    @IBAction func onStartButtonPressed(_ sender: Any)
+    @IBOutlet weak var backgroundView: UIView!
+    var pattern = [Int]()
+    var timer = Timer()
+    var index = 0
     
+    override func viewDidLoad()
     {
-        flashColor(number: 0)
+        super.viewDidLoad()
     }
     
-  
-    
-    
+    @IBAction func onStartButtonPressed(_ sender: Any)
+        
+    {
+        for _ in 0...10 {
+            addToPattern()
+        }
+        
+        displayPattern()
+    }
     
     
     func flashColor(number:Int)
     {
         UIView.transition(with: colorDisplays[number], duration: 0.5, options: .transitionCrossDissolve, animations: {
-            self.colorDisplays[number]
-        }, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
-        UIView.transition(with: colorDisplays[number], duration:0.5 , options: .transitionCrossDissolve, animations:
-            {
             self.colorDisplays[number].alpha = 1.0
-        }, completion: nil)
+        }) { (true) in
+            UIView.transition(with: self.colorDisplays[number], duration:0.7 , options: .transitionCrossDissolve, animations:
+                {
+                    self.colorDisplays[number].alpha = 0.4
+            }, completion: nil)
+        }
+        
     }
-
+    func displayPattern()
+    {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: (#selector(ViewController.nextColor)), userInfo: nil, repeats: true)
+    }
     
+    @objc func nextColor()
+    {
+        if index < pattern.count
+        {
+            flashColor(number: pattern[index])
+            index += 1
+        }
+        else
+        {
+            timer.invalidate()
+            index = 0
+        }
+    }
+    public func addToPattern()
+    {
+        pattern.append(Int(arc4random_uniform(4)))
+    }
     
+    @IBAction func onColorTapped(_ sender: UITapGestureRecognizer)
+    {
+        for number in 0..<colorDisplays.count
+        {
+            if colorDisplays[number].frame.contains(sender.location(in: backgroundView))
+            {
+                flashColor(number: number)
+                
+            }
+        }
+    }
 }
 
